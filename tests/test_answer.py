87dee041c09f6e73e1079b9e599d027f0aaa9b9c
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2024-02-27 22:54:14 +0300

    fix: Fixed tests

--- tests/test_answer.py
+++ tests/test_answer.py
@@ -1,20 +1,41 @@
 import pytest
 from src.player import Player
 
+from os import getenv
+from ujson import load
+
+
+PROJECT_DIR = getenv("PROJECT_DIR")
+
+
+with open(f"{PROJECT_DIR}/game_data/resources/races.json", "r", encoding="utf-8") as file:
+    RACES = load(file)
+with open(f"{PROJECT_DIR}/game_data/resources/classes.json", "r", encoding="utf-8") as file:
+    CLASSES = load(file)
+
 
 @pytest.fixture()
 def player():
-    data = {
-        "id": 0,
-        "first_name": "empty",
-        "last_name": "empty",
-        "full_name": "empty",
-        "username": "empty",
-        "state": "Choosing a race"
-    }
+    global PROJECT_DIR
+    data = {}
+    with open(f"{PROJECT_DIR}/game_data/resources/zero_player.json", "r", encoding="utf-8") as file:
+        zero_player = load(file)
+        for i in zero_player:
+            data[i] = zero_player[i]["val"]
     return Player(data)
 
 
 def test_answer(player: Player):
     answer, _, _ = player.process_request("/start")
-    assert "Unknown action" == answer
+    assert "Choose a race" == answer
+
+
+def test_buttons(player: Player):
+    global RACES
+    _, _, buttons = player.process_request("/start")
+    assert [[i] for i in RACES] == buttons
+
+
+def test_image(player: Player):
+    _, image, _ = player.process_request("/start")
+    assert f"{PROJECT_DIR}/game_data/images/fantasy-races.png" == image
